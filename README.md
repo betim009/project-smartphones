238942deb8125147f7631bbb6c462e4bda1c1caa
Alberto <betim009@gmail.com>
2024-04-01 17:28:42 -0300

    banco de dados - tabela db.sql

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # project-smartphones
+
+
+
+
+
+## Primeiros passos:
+
+1. `npm init -y`
+2. criando banco de dados
+3. script no arquivo package.json `createdb`
+
+
---
+++ backend/db.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS smartphones;
+
+CREATE DATABASE smartphones;
+
+USE smartphones;
+
+CREATE TABLE phones (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(60) NOT NULL,
+  brand VARCHAR(80) NOT NULL,
+  model VARCHAR(100) NOT NULL,
+  idade INT NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  color VARCHAR(50) NOT NULL
+);
