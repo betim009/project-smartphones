7ac2500ce6b8d5e815dc166fd366cb8e59bba7f8
Alberto <betim009@gmail.com>
2024-04-01 18:01:45 -0300

    primeira rota getAll phones

--- README.md
+++ README.md
@@ -9,5 +9,7 @@
 1. `npm init -y`
 2. criando banco de dados
 3. script no arquivo package.json `createdb`
-
+4. criar o seed do banco de dados e script `seeddb`
+5. estruturar pastas do projeto
+6. criar connection.js, app.js e server.js
 
--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+const connection = require('./db/connection');
+
 const express = require('express');
 const cors = require('cors');
 
@@ -5,5 +7,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// rota phones
+app.get('/phones', async (req, res) => {
+    const [results] = await connection.execute(
+        'SELECT * FROM phones'
+    );
+    
+    const data = {
+        "total": results.length,
+        "results": results
+    }    
+
+    return res.status(200).json(data);
+})
 
 module.exports = app;
