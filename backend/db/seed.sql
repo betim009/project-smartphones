c1aa0bbb09f4515b71455efca79274e3a28ac5bf
Alberto <betim009@gmail.com>
2024-04-02 12:26:03 -0300

    atualizando bd

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -13,6 +13,19 @@ VALUES
         'Iphone 14 Pro',
         'Iphone',
         '14 Pro',
+        10050.99,
+        'silver'
+    ),
+        (
+        'Iphone 14',
+        'Iphone',
+        '14',
         4050.99,
         'silver'
     );
+
+INSERT INTO
+    sales (phone_id, sale_date, quantity, total_price)
+VALUES
+    (1, '2024-04-01', 2, 2899.98),
+    (2, '2024-04-02', 1, 10050.99);
