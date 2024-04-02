d9a33a06ec586ec43cd6e527de6d9c978b52c21f
Alberto <betim009@gmail.com>
2024-04-02 10:36:17 -0300

    criando tabela sales

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -4,11 +4,22 @@ CREATE DATABASE smartphones;
 
 USE smartphones;
 
-CREATE TABLE phones (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(60) NOT NULL,
-  brand VARCHAR(80) NOT NULL,
-  model VARCHAR(100) NOT NULL,
-  price DECIMAL(10, 2) NOT NULL,
-  color VARCHAR(50) NOT NULL
-);
+CREATE TABLE
+  phones (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    brand VARCHAR(80) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    color VARCHAR(50) NOT NULL
+  );
+
+CREATE TABLE
+  sales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    phone_id INT NOT NULL,
+    sale_date DATE NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (phone_id) REFERENCES phones (id)
+  );
