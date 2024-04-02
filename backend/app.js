cd40c42fd6efece3fd116bfcd00f5717010eec9e
Alberto <betim009@gmail.com>
2024-04-01 22:39:03 -0300

    rota post

--- backend/app.js
+++ backend/app.js
@@ -13,13 +13,33 @@ app.get('/phones', async (req, res) => {
     const [results] = await connection.execute(
         'SELECT * FROM phones'
     );
-    
+
     const data = {
         "total": results.length,
         "results": results
     };
 
     return res.status(200).json(data);
-})
+});
+
+app.post('/phones', async (req, res) => {
+    const { name, brand, model, price, color } = req.body;
+
+    const [results] = await connection.execute(
+        'INSERT INTO phones (name, brand, model, price, color) VALUES (?, ?, ?, ?, ?)',
+        [name, brand, model, price, color]);
+
+    return res.status(200).json({"result": "Create phone"});
+
+
+});
+
+app.put('/phones', async () => {
+
+});
+
+app.del('/phones', async () => {
+
+});
 
 module.exports = app;
