13e711b6722a3cbda49845b5da638634d2af7ca2
Alberto <betim009@gmail.com>
2024-09-04 10:22:27 -0300

    feat: outled

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<h2>!</h2>}>
-          <Route index element={<h2>!</h2>} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </>
